09e8f04b8df52c6fcc897427b7d6d0a398edd3cf
Quentin FAYET <44276782+fidjy@users.noreply.github.com>
2020-02-10 11:38:43 +0100

    add sql

---
+++ help.sql
@@ -0,0 +1,43 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Généré le :  lun. 10 fév. 2020 à 09:45
+-- Version du serveur :  10.4.10-MariaDB
+-- Version de PHP :  7.4.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `help`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `card_template`
+--
+
+DROP TABLE IF EXISTS `card_template`;
+CREATE TABLE IF NOT EXISTS `card_template` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `active` tinyint(4) NOT NULL,
+  `premium` tinyint(4) NOT NULL,
+  `preview` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
